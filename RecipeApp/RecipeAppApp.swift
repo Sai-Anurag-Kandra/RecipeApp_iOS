5d83ad9a5f2b5eb0963618e4593323c050cf73e7
rickyajith <ajithgolla@gmail.com>
2024-07-25 01:48:42 -0400

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Ricky Ajith Golla on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
